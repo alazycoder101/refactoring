effa2fa61c5a51c4436312d95e258f49cfe1551d
lazycoder <alazycoder101@gmail.com>
2021-12-04 01:26:06 +0000

    add ruby version of theatrical-players

---
+++ README.md
@@ -0,0 +1,11 @@
+# Refactoring
+Theatrical Players Refactoring Kata
+
+# Reference
+https://github.com/emilybache/Theatrical-Players-Refactoring-Kata
+https://github.com/raddanesh/Kata01
+http://codekata.com/kata/kata01-supermarket-pricing/
+https://github.com/emilybache/GildedRose-Refactoring-Kata
+https://github.com/emilybache/Tennis-Refactoring-Kata
+https://github.com/baile320/refactoring
+https://github.com/arnau-rius/theatrical-players-refactoring
---
+++ theatrical-players/ruby/statement.rb
@@ -0,0 +1,47 @@
+require 'json'
+def parse(file)
+  json = File.read(file)
+  object = JSON.parse(json, object_class: OpenStruct)
+end
+
+invoices = parse('../invoices.json')
+plays = parse('../plays.json')
+
+def statement(invoice, plays)
+    totalAmount = 0
+    volumeCredits = 0
+    result = "Statement for #{invoice.customer}\n\t"
+
+
+    invoice.performances.each do |perf|
+      play = plays[perf.playID]
+      thisAmount = 0
+      case play.type
+      when "tragedy"
+        thisAmount = 40000
+        if (perf.audience > 30)
+          thisAmount += 1000 * (perf.audience - 30)
+        end
+      when "comedy"
+        thisAmount = 30000
+        if (perf.audience > 20)
+          thisAmount += 10000 + 500 * (perf.audience - 20)
+        end
+        thisAmount += 300 * perf.audience
+      else
+        raise "unknown type: #{play.type}"
+        # add volume credits
+        volumeCredits += [perf.audience - 30, 0].max
+        # add extra credit for every ten comedy attendees
+        volumeCredits += (perf.audience / 5.0).floor if "comedy" === play.type
+        # print line for this order
+        result += "#{play.name}: #{thisAmount / 100.0} (#{perf.audience} seats)\n\t"
+        totalAmount += thisAmount
+      end
+    end
+    result += "Amount owed is #{(totalAmount / 100.0)}\n"
+    result += "You earned #{volumeCredits} credits\n"
+    result
+end
+
+puts statement(invoices[0], plays)
